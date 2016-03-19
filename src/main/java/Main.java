28d4b96a4a3de7c55d415b978ee1598c1bfba74c
Kirill <neikila@gmail.com>
2016-03-19 19:54:10 +0300

    Add comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,20 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        byte[] bytesFromFile = null;
-        String filename = "Bugreport.png";
+        byte[] bytesFromFile = null;        // Переменная для хранения массива байт
+        String filename = "Bugreport.png";  // Путь до файла из корня
 
         try {
+            // Получение массива байт из картинки
             bytesFromFile = Files.readAllBytes(Paths.get(filename));
 
+            // Вывод в консоль массива байт, если убрать "(char)", то увидишь просто поток цифр.
+            // В консоли картинки ты никак не получишь.
             for (int i = 0; i < bytesFromFile.length; ++i) {
                 System.out.print((char)bytesFromFile[i]);
             }
 
+            // Запись в НОВЫЙ файл путь до которого someFile.png
             FileOutputStream fos = new FileOutputStream(new File("someFile.png"));
             fos.write(bytesFromFile);
             fos.close();
