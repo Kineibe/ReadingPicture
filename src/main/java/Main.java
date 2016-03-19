ddcef3d89de15264cdb6865ca50ca9731c10b52d
Kirill <neikila@gmail.com>
2016-03-19 20:22:57 +0300

    With list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by Neikila on 19.03.2016.
@@ -18,13 +21,19 @@ public class Main {
 
             // Вывод в консоль массива байт, если убрать "(char)", то увидишь просто поток цифр.
             // В консоли картинки ты никак не получишь.
+
+            List <Integer> list = new ArrayList<Integer>();
             for (int i = 0; i < bytesFromFile.length; ++i) {
                 System.out.print((char)bytesFromFile[i]);
+                list.add((int)bytesFromFile[i]);
             }
 
             // Запись в НОВЫЙ файл путь до которого someFile.png
-            FileOutputStream fos = new FileOutputStream(new File("someFile.png"));
-            fos.write(bytesFromFile);
+            final FileOutputStream fos = new FileOutputStream(new File("someFile.png"));
+            for (Integer num : list) {
+                fos.write(num);
+            }
+//            fos.write(bytesFromFile);
             fos.close();
         } catch (Exception e) {
             e.printStackTrace();
